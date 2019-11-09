9ac08e76fb5634de07a16c1b377f167595689ff2
LuckieLuke <luk19984@gmail.com>
2019-11-09 17:04:55 +0100

    Correctly creating maze

---
+++ MazeSolver/src/Main/Main.java
@@ -0,0 +1,57 @@
+package Main;
+
+import Maze.Maze;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    static Maze m;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        FileManager fm = new FileManager();
+        System.out.println("How to create a maze?");
+        System.out.println("1) by size");
+        System.out.println("2) by reading from file");
+        String answer = sc.next();
+
+        if(answer.equals("1")) {
+            System.out.println("What size? (height width)");
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            m = new Maze(h, w);
+            m.generate(1, m.getWidth()-2, 1, m.getHeight()-2, m.getWidth() > m.getHeight());
+        }
+        else if(answer.equals("2")) {
+            System.out.println("Path to the file:");
+            String path = sc.next();
+
+            m = new Maze(path, fm);
+        }
+        else {
+            System.out.println("Cannot continue - wrong option!");
+            System.exit(1);
+        }
+
+        System.out.println("Want to save the maze? (yes/no)");
+        answer = sc.next();
+        if(answer.equals("yes")) {
+            System.out.println("Give path to the file:");
+            String path = sc.next();
+            try {
+                fm.writeData(path, m);
+            }
+            catch(IOException e) {
+                System.out.println("Cannot save to the file!");
+                System.exit(3);
+            }
+            System.out.println("Saved!");
+        }
+
+        System.out.println(m);
+
+    }
+
+}
